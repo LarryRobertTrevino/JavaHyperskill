fbae3bfde0965de0bc6a65c882f9bf73b4954301
LarryRobertTrevino <55628198+LarryRobertTrevino@users.noreply.github.com>
2020-04-24 17:31:42 +0300

    Update Main.java
    stage 2 complete

--- Simple Search Engine/search/Main.java
+++ Simple Search Engine/search/Main.java
@@ -1,21 +1,101 @@
 package search;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        List<person> data = enterData();
+        search(data);
+    }
+
+    public static List<person> enterData() {
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split("\\s+");
-        String search = scanner.nextLine();
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < input.length; i++) {
-            if (input[i].equals(search)) {
-                answer = i + 1;
-                break;
+        System.out.println("Enter the number of people:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        List<person> data = new ArrayList<>();
+        System.out.println("Enter all people:");
+        for (int i = 0; i < n; i++) {
+            String[] temp = scanner.nextLine().split("\\s+");
+            switch (temp.length) {
+                case 2:
+                    data.add(new person(temp[0],temp[1]));
+                    break;
+                case 3:
+                    data.add(new person(temp[0],temp[1],temp[2]));
+                default:
+                    break;
             }
         }
-        if (answer == Integer.MIN_VALUE) {
-            System.out.println("Not Found");
-        } else System.out.println(answer);
+        System.out.println();
+        return data;
+    }
+
+    public static void search(List<person> data) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of search queries:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+        String[] queries = new String[n];
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            List<person> founded = new LinkedList<>();
+            System.out.println("Enter data to search people:");
+            queries[i] = scanner.nextLine();
+            for( person a : data) {
+                if (a.firstName.equalsIgnoreCase(queries[i]) || a.lastName.equalsIgnoreCase(queries[i])) {
+                    founded.add(a);
+                    found = true;
+                    continue;
+                }
+                if (a.email != null) {
+                    if (queries[i].equals("@")) {
+                        founded.add(a);
+                        found = true;
+                        continue;
+                    }
+                    String[] partOfEmail = a.email.split("\\W");
+                    for (String b : partOfEmail) {
+                        if (b.equalsIgnoreCase(queries[i])) {
+                            founded.add(a);
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!found) {
+                System.out.println("No matching people found.");
+            } else {
+                System.out.println();
+                System.out.println("Found people:");
+                for (person a : founded) {
+                    System.out.println(a.toString());
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class person {
+    String firstName;
+    String lastName;
+    String email;
+
+    public person(String firstName, String lastName) {
+        this(firstName, lastName, null);
+    }
+
+    public person(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " " + email;
     }
 }
