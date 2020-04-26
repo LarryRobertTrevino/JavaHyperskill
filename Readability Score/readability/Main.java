f67108f4a11391e773285372d127cee2b42293ad
LarryRobertTrevino <55628198+LarryRobertTrevino@users.noreply.github.com>
2020-04-26 16:33:46 +0300

    Update Main.java

--- Readability Score/readability/Main.java
+++ Readability Score/readability/Main.java
@@ -5,9 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        if (input.length() > 100) {
+        String[] input = scanner.nextLine().split("\\s+");
+        double wordsCounts = 1;
+        double sentencesCounts = 1;
+        for (int i = input.length - 2; i >= 0; i--) {
+            if (input[i].matches(".+[.!?]")) {
+                sentencesCounts++;
+            }
+            wordsCounts++;
+        }
+        double estimate = wordsCounts / sentencesCounts;
+        if (estimate > 10) {
             System.out.println("HARD");
         } else System.out.println("EASY");
+
     }
 }
