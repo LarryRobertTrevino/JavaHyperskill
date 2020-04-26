65c1ac535c7cef163dacc34cfd3e2e2f3c75d8d7
LarryRobertTrevino <55628198+LarryRobertTrevino@users.noreply.github.com>
2020-04-26 14:20:40 +0300

    Update Main.java
    project completed //future refactoring

--- Simple Search Engine/search/Main.java
+++ Simple Search Engine/search/Main.java
@@ -3,14 +3,15 @@ package search;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Map<String, Set<Integer>> searchMap = new LinkedHashMap<>();
         File inputData = new File(args[1]);
-        List<person> data = enterData(inputData, searchMap);
+        List<Person> data = enterData(inputData, searchMap);
+        AllSearcher allSearcher = new AllSearcher(data, searchMap);
+        AnySearcher anySearcher = new AnySearcher(data, searchMap);
+        NoneSearcher noneSearcher = new NoneSearcher(data, searchMap);
         Scanner scanner = new Scanner(System.in);
         boolean go = true;
         while (go) {
@@ -19,10 +20,24 @@ public class Main {
             scanner.nextLine();
             switch (answer) {
                 case 1:
-                    searchByInvertedIndex(data, searchMap);
+                    System.out.println();
+                    System.out.println("Select a matching strategy: ALL, ANY, NONE");
+                    switch (scanner.nextLine()) {
+                        case "ALL":
+                            allSearcher.search();
+                            break;
+                        case "ANY":
+                            anySearcher.search();
+                            break;
+                        case "NONE":
+                            noneSearcher.search();
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case 2:
-                    for (person a : data) {
+                    for (Person a : data) {
                         System.out.println(a.toString());
                     }
                     break;
@@ -45,27 +60,28 @@ public class Main {
         System.out.println("0. Exit");
     }
 
-    public static List<person> enterData(File file, Map<String, Set<Integer>> searchMap) {
-        List<person> data = new ArrayList<>();
+    public static List<Person> enterData(File file, Map<String, Set<Integer>> searchMap) {
+        List<Person> data = new ArrayList<>();
         int lineCount = 0;
         try(Scanner scanner = new Scanner(file)) {
             while (scanner.hasNext()) {
                 String[] temp = scanner.nextLine().split("\\s+");
                 switch (temp.length) {
                     case 2:
-                        data.add(new person(temp[0],temp[1]));
+                        data.add(new Person(temp[0],temp[1]));
                         break;
                     case 3:
-                        data.add(new person(temp[0],temp[1],temp[2]));
+                        data.add(new Person(temp[0],temp[1],temp[2]));
                     default:
                         break;
                 }
 
                 for (String a : temp) {
-                    if (!searchMap.containsKey(a)) {
-                        searchMap.put(a, new LinkedHashSet<>());
+                    String b = a.toLowerCase();
+                    if (!searchMap.containsKey(b)) {
+                        searchMap.put(b, new LinkedHashSet<>());
                     }
-                    searchMap.get(a).add(lineCount);
+                    searchMap.get(b).add(lineCount);
                 }
                 lineCount++;
             }
@@ -74,59 +90,19 @@ public class Main {
         }
         return data;
     }
-
-    public static void searchByInvertedIndex(List<person> data, Map<String, Set<Integer>> searchMap) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a name or email to search all suitable people.");
-        String request = scanner.nextLine();
-        if (searchMap.get(request) == null) {
-            System.out.println("No matching people found.");
-            System.out.println();
-            return;
-        }
-        for (Integer a : searchMap.get(request)) {
-            System.out.println("" + searchMap.get(request).size() + "persons found:");
-            System.out.println(data.get(a).toString());
-        }
-    }
-
-    public static void search(List<person> data) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a name or email to search all suitable people.");
-        String request = scanner.nextLine();
-        Pattern pattern = Pattern.compile(request, Pattern.CASE_INSENSITIVE);
-        boolean found = false;
-        List<person> founded = new LinkedList<>();
-        for( person a : data) {
-            Matcher matcher = pattern.matcher(a.toString());
-            if (matcher.find()) {
-                found = true;
-                founded.add(a);
-            }
-        }
-        if (!found) {
-            System.out.println("No matching people found.");
-        } else {
-            System.out.println();
-            System.out.println("Found people:");
-            for (person a : founded) {
-                System.out.println(a.toString());
-            }
-        }
-        System.out.println();
-    }
 }
 
-class person {
+class Person {
+
     String firstName;
     String lastName;
     String email;
 
-    public person(String firstName, String lastName) {
+    public Person(String firstName, String lastName) {
         this(firstName, lastName, null);
     }
 
-    public person(String firstName, String lastName, String email) {
+    public Person(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -140,3 +116,96 @@ class person {
     }
 }
 
+abstract class Searcher {
+
+    Set<Integer> searchResult;
+    String[] queries;
+    List<Person> data;
+    Map<String, Set<Integer>> searchMap;
+
+    Searcher(List<Person> data, Map<String, Set<Integer>> searchMap) {
+        this.data = data;
+        this.searchMap = searchMap;
+    }
+
+    public void search() {
+        getQuery();
+        searchingProcess();
+        printResult();
+    }
+
+    protected void getQuery() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name or email to search all suitable people.");
+        queries = scanner.nextLine().toLowerCase().split("\\s+");
+        scanner.close();
+        searchResult = new LinkedHashSet<>();
+    }
+
+    protected void printResult(){
+        if (searchResult.size() > 0) {
+        System.out.println("" + searchResult.size() + " persons found:");
+        for (Integer a : searchResult) {
+            System.out.println(data.get(a).toString());
+        }
+        } else System.out.println("No matching people found.");
+    }
+
+    protected abstract void searchingProcess();
+}
+
+class AnySearcher extends Searcher {
+
+    AnySearcher(List<Person> data, Map<String, Set<Integer>> searchMap) {
+        super(data, searchMap);
+    }
+
+    @Override
+    protected void searchingProcess() {
+        for (String a : queries) {
+            if (searchMap.get(a) != null) {
+                searchResult.addAll(searchMap.get(a));
+            }
+        }
+    }
+}
+
+class AllSearcher extends Searcher {
+
+    AllSearcher(List<Person> data, Map<String, Set<Integer>> searchMap) {
+        super(data, searchMap);
+    }
+
+    @Override
+    protected void searchingProcess() {
+        for (String a : queries) {
+            if (searchMap.get(a) != null) {
+                searchResult.addAll(searchMap.get(a));
+            }
+        }
+        for (String a : queries) {
+            if (searchMap.get(a) != null) {
+                searchResult.retainAll(searchMap.get(a));
+            }
+        }
+    }
+}
+
+class NoneSearcher extends Searcher {
+
+    NoneSearcher(List<Person> data, Map<String, Set<Integer>> searchMap) {
+        super(data, searchMap);
+    }
+
+    @Override
+    protected void searchingProcess() {
+        for (int i = 0; i < data.size(); i++) {
+            searchResult.add(i);
+        }
+        for (String a : queries) {
+            if (searchMap.get(a) != null) {
+                searchResult.removeAll(searchMap.get(a));
+            }
+        }
+    }
+}
