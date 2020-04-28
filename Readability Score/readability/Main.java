d9e67861e5386861a667d1cff28002868ce8d849
LarryRobertTrevino <55628198+LarryRobertTrevino@users.noreply.github.com>
2020-04-28 18:00:37 +0300

    Update Main.java

--- Readability Score/readability/Main.java
+++ Readability Score/readability/Main.java
@@ -1,23 +1,13 @@
 package readability;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split("\\s+");
-        double wordsCounts = 1;
-        double sentencesCounts = 1;
-        for (int i = input.length - 2; i >= 0; i--) {
-            if (input[i].matches(".+[.!?]")) {
-                sentencesCounts++;
-            }
-            wordsCounts++;
-        }
-        double estimate = wordsCounts / sentencesCounts;
-        if (estimate > 10) {
-            System.out.println("HARD");
-        } else System.out.println("EASY");
-
+    public static void main(String[] args) throws IOException {
+        String text = new String(Files.readAllBytes(Paths.get(args[0])));
+        AutomatedReadabilityIndex automatedReadabilityIndex = new AutomatedReadabilityIndex(text);
+        automatedReadabilityIndex.printResult();
     }
 }
